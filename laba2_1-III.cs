5cbcba3506979d8363ae8c0c881931296392e3da
Максим <56740030+Maxim1720@users.noreply.github.com>
2020-03-07 18:56:34 +0400

    Добавление всех исходников

---
+++ laba2_1-III.cs
@@ -0,0 +1,12 @@
+﻿using System;
+class Sanek
+{
+    static void Main()
+    {
+        Console.Write("enter the num:");
+        int x = Convert.ToInt32(Console.ReadLine());
+        for (int i = 0; i < 3; i++, x *= x) ;
+        Console.WriteLine("x^8 = " + x);
+    }
+}
+
